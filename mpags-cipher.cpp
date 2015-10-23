5cc6400fae40e8014aca17b781db8a91cf9ca8cc
Kristian Zarebski <kaz@hep.ph.bham.ac.uk>
2015-10-23 14:21:22 +0100

    Source code updated to include variables exercise

--- mpags-cipher.cpp
+++ mpags-cipher.cpp
@@ -1,11 +1,25 @@
 //! Main implementation of the mpags-cipher program
 #include <iostream>
+#include <string>
 // This is one of the two valid signatures for main()
 int main(int /*argc*/, char* /*argv*/[]) {
-	  // No requirement to return from main, but we do so for clarity and
-	  // consistency with other functions.
 
-  std::cout << "Hello World! \n";
+	int a{10}, e{11};
+	const double b{334.567};
+	int c{b};
+	auto d{true};
+	std::string f{"BOOM"};	
 
-  return 0;
+	char g{f[2]};
+
+	a = 20;
+
+	std::cout << "Hello World! \n";
+	std::cout << "The integer value you arbitrarily picked to state is: " << a << std::endl;
+	std::cout << "The non random number of the day is: " << b << std::endl;
+	std::cout << "Divide a double by an int and you get: " << b/a << std::endl;
+	std::cout << "DON'T divide an int by an int: " << a/e << " <- loss of precision!" << std::endl;
+	std::cout << "Life is pretty " << f << std::endl;
+	std::cout << "Not so random character is '" << g << "' \n";
+	return 0;
 }
